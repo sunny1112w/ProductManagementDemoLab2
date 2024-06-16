1e0a81162d482f2a8879a0705d593bca4fba80d3
ADMIN <ADMIN@LAPTOP-SS809F44>
2024-06-16 11:14:50 +0700

    Add project files.

---
+++ BusinessObjects/Migrations/20240614173337_Initial.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObjects.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountMember",
+                columns: table => new
+                {
+                    MemberID = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    MemberPassword = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    MemberRole = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__AccountM__0CF04B38E2385640", x => x.MemberID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Categori__19093A2BBE56153A", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    CategoryID = table.Column<int>(type: "int", nullable: true),
+                    UnitsInStock = table.Column<short>(type: "smallint", nullable: true),
+                    UnitPrice = table.Column<decimal>(type: "money", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Products__B40CC6EDD85AB4A2", x => x.ProductID);
+                    table.ForeignKey(
+                        name: "FK__Products__Catego__29572725",
+                        column: x => x.CategoryID,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__AccountM__49A147409836A3A5",
+                table: "AccountMember",
+                column: "EmailAddress",
+                unique: true,
+                filter: "[EmailAddress] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryID",
+                table: "Products",
+                column: "CategoryID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountMember");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
